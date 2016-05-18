ea62a64e4532ddbfc6e85b29e6bde00b8105d951
Hendras92 <hendras92@gmail.com>
2016-05-18 10:45:59 +0700

    Commit Kedua

---
+++ application/views/V_manage_event.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>E-Voting Fasilkom Unsri</title>
+		<link rel="shortcut icon" href="<?php echo base_url();?>asset/img/ui-26/evotingfasilkom.png"/>
+		<!-- Description, Keywords and Author -->
+		<meta name="description" content="Your description">
+		<meta name="keywords" content="Your,Keywords">
+		<meta name="author" content="ResponsiveWebInc">
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<!-- Styles -->
+		<!-- Bootstrap CSS -->
+		<link href="<?php echo base_url();?>asset/css/bootstrap.min.css" rel="stylesheet">
+		<!-- Font awesome CSS -->
+		<link href="<?php echo base_url();?>asset/css/font-awesome.min.css" rel="stylesheet">
+		
+		
+		<!-- Main CSS -->
+		<link href="<?php echo base_url();?>asset/css/style-199.css" rel="stylesheet">
+				
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="#">
+	</head>
+	<body>
+	
+		
+		<!-- UI # -->
+		<div class="ui-199">
+		
+			<h4><i class="fa fa-file red"></i>&nbsp; List Vote</h4>
+		
+			<div class="table-responsive">
+				<table class="table table-striped">
+					<tr>
+						<th>#</th>
+						<th>File Name</th>
+						<th>Size</th>
+						<th>Created</th>
+						<th>Author</th>
+						<th>Actions</th>
+					</tr>
+					<tr>
+						<!-- File icon -->
+						<td><i class="fa fa-file-image-o red"></i></td>
+						<!-- File name -->
+						<td><a href="#">Camping Pictures</a></td>
+						<!-- File size -->
+						<td>2.49 MB</td>
+						<!-- Date created -->
+						<td>02/02/2014</td>
+						<!-- Author -->
+						<td><a href="#"><img src="<?php echo base_url();?>asset/img/ui-199/4.jpg" alt="" class="img-responsive" /></a></td>
+						<!-- Actions -->
+						<td>
+							<button type="button" class="btn btn-green btn-xs"><i class="fa fa-download"></i></button>
+							<button type="button" class="btn btn-red btn-xs"><i class="fa fa-trash"></i></button>
+							<button type="button" class="btn btn-lblue btn-xs"><i class="fa fa-pencil"></i></button>
+						</td>
+					</tr>
+					<tr>
+						<!-- File icon -->
+						<td><i class="fa fa-file-sound-o red"></i></td>
+						<!-- File name -->
+						<td><a href="#">Baby Doll mp3</a></td>
+						<!-- File size -->
+						<td>2.49 MB</td>
+						<!-- Date created -->
+						<td>02/02/2014</td>
+						<!-- Author -->
+						<td><a href="#"><img src="<?php echo base_url();?>asset/img/ui-199/3.jpg" alt="" class="img-responsive" /></a></td>
+						<!-- Actions -->
+						<td>
+							<button type="button" class="btn btn-green btn-xs"><i class="fa fa-download"></i></button>
+							<button type="button" class="btn btn-red btn-xs"><i class="fa fa-trash"></i></button>
+							<button type="button" class="btn btn-lblue btn-xs"><i class="fa fa-pencil"></i></button>
+						</td>
+					</tr>
+					<tr>
+						<!-- File icon -->
+						<td><i class="fa fa-file-powerpoint-o red"></i></td>
+						<!-- File name -->
+						<td><a href="#">Biscuit manufacturing ppt</a></td>
+						<!-- File size -->
+						<td>2.49 MB</td>
+						<!-- Date created -->
+						<td>02/02/2014</td>
+						<!-- Author -->
+						<td><a href="#"><img src="<?php echo base_url();?>asset/img/ui-199/2.jpg" alt="" class="img-responsive" /></a></td>
+						<!-- Actions -->
+						<td>
+							<button type="button" class="btn btn-green btn-xs"><i class="fa fa-download"></i></button>
+							<button type="button" class="btn btn-red btn-xs"><i class="fa fa-trash"></i></button>
+							<button type="button" class="btn btn-lblue btn-xs"><i class="fa fa-pencil"></i></button>
+						</td>
+					</tr>
+					<tr>
+						<!-- File icon -->
+						<td><i class="fa fa-file-pdf-o red"></i></td>
+						<!-- File name -->
+						<td><a href="#">Statemtn Feb 2015</a></td>
+						<!-- File size -->
+						<td>2.49 MB</td>
+						<!-- Date created -->
+						<td>02/02/2014</td>
+						<!-- Author -->
+						<td><a href="#"><img src="<?php echo base_url();?>asset/img/ui-199/1.jpg" alt="" class="img-responsive" /></a></td>
+						<!-- Actions -->
+						<td>
+							<button type="button" class="btn btn-green btn-xs"><i class="fa fa-download"></i></button>
+							<button type="button" class="btn btn-red btn-xs"><i class="fa fa-trash"></i></button>
+							<button type="button" class="btn btn-lblue btn-xs"><i class="fa fa-pencil"></i></button>
+						</td>
+					</tr>
+					<tr>
+						<!-- File icon -->
+						<td><i class="fa fa-file-excel-o red"></i></td>
+						<!-- File name -->
+						<td><a href="#">Employees attendance</a></td>
+						<!-- File size -->
+						<td>2.49 MB</td>
+						<!-- Date created -->
+						<td>02/02/2014</td>
+						<!-- Author -->
+						<td><a href="#"><img src="<?php echo base_url();?>asset/img/ui-199/4.jpg" alt="" class="img-responsive" /></a></td>
+						<!-- Actions -->
+						<td>
+							<button type="button" class="btn btn-green btn-xs"><i class="fa fa-download"></i></button>
+							<button type="button" class="btn btn-red btn-xs"><i class="fa fa-trash"></i></button>
+							<button type="button" class="btn btn-lblue btn-xs"><i class="fa fa-pencil"></i></button>
+						</td>
+					</tr>
+					<tr>
+						<!-- File icon -->
+						<td><i class="fa fa-file-word-o red"></i></td>
+						<!-- File name -->
+						<td><a href="#">How to write a book</a></td>
+						<!-- File size -->
+						<td>2.49 MB</td>
+						<!-- Date created -->
+						<td>02/02/2014</td>
+						<!-- Author -->
+						<td><a href="#"><img src="<?php echo base_url();?>asset/img/ui-199/3.jpg" alt="" class="img-responsive" /></a></td>
+						<!-- Actions -->
+						<td>
+							<button type="button" class="btn btn-green btn-xs"><i class="fa fa-download"></i></button>
+							<button type="button" class="btn btn-red btn-xs"><i class="fa fa-trash"></i></button>
+							<button type="button" class="btn btn-lblue btn-xs"><i class="fa fa-pencil"></i></button>
+						</td>
+					</tr>
+					<tr>
+						<!-- File icon -->
+						<td><i class="fa fa-file-text-o red"></i></td>
+						<!-- File name -->
+						<td><a href="#">Latest website logs</a></td>
+						<!-- File size -->
+						<td>2.49 MB</td>
+						<!-- Date created -->
+						<td>02/02/2014</td>
+						<!-- Author -->
+						<td><a href="#"><img src="<?php echo base_url();?>asset/img/ui-199/2.jpg" alt="" class="img-responsive" /></a></td>
+						<!-- Actions -->
+						<td>
+							<button type="button" class="btn btn-green btn-xs"><i class="fa fa-download"></i></button>
+							<button type="button" class="btn btn-red btn-xs"><i class="fa fa-trash"></i></button>
+							<button type="button" class="btn btn-lblue btn-xs"><i class="fa fa-pencil"></i></button>
+						</td>
+					</tr>
+					<tr>
+						<!-- File icon -->
+						<td><i class="fa fa-file-image-o red"></i></td>
+						<!-- File name -->
+						<td><a href="#">Pics of my dog and cat</a></td>
+						<!-- File size -->
+						<td>2.49 MB</td>
+						<!-- Date created -->
+						<td>02/02/2014</td>
+						<!-- Author -->
+						<td><a href="#"><img src="<?php echo base_url();?>asset/img/ui-199/1.jpg" alt="" class="img-responsive" /></a></td>
+						<!-- Actions -->
+						<td>
+							<button type="button" class="btn btn-green btn-xs"><i class="fa fa-download"></i></button>
+							<button type="button" class="btn btn-red btn-xs"><i class="fa fa-trash"></i></button>
+							<button type="button" class="btn btn-lblue btn-xs"><i class="fa fa-pencil"></i></button>
+						</td>
+					</tr>
+					<tr>
+						<th></th>
+						<th>15 Files</th>
+						<th>250 MB</th>
+						<th></th>
+						<th></th>
+						<th></th>
+					</tr>
+				</div>
+			</div>
+		
+		</div>
+		
+		<!-- Javascript files -->
+		<!-- jQuery -->
+		<script src="<?php echo base_url();?>asset/js/jquery.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="<?php echo base_url();?>asset/js/bootstrap.min.js"></script>
+		<!-- Placeholder JS -->
+		<script src="<?php echo base_url();?>asset/js/placeholder.js"></script>
+		<!-- Respond JS for IE8 -->
+		<script src="<?php echo base_url();?>asset/js/respond.min.js"></script>
+		<!-- HTML5 Support for IE -->
+		<script src="<?php echo base_url();?>asset/js/html5shiv.js"></script>
+	</body>	
+</html>
